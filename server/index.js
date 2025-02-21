524c923210a9026665cc4fc540439a559070953a
Mansi-298 <166313202+Mansi-298@users.noreply.github.com>
2025-02-21 16:43:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use("/api/v1/progress", courseProgressRoute);
  
 app.use(express.static(path.join(_dirname, "/client/dist")))
 app.get('*', (_, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
